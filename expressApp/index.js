04e7e4fdb0657fcae5a0f9dda3d0c490c15ffff9
Poreniyi <poreniyi@gmail.com>
2021-01-20 18:40:23 -0500

    set views path and fixed path to public directory

--- expressApp/index.js
+++ expressApp/index.js
@@ -1,11 +1,20 @@
 let express=require('express');
 let app=express();
-let loginRoute= require('./routes/home');
+let path=require('path');
+let loginRoute= require('./routes/login');
+let navBar=require('./routes/home');
 
-
+//routing
 app.use('/',loginRoute);
-app.use(express.static('./public'));
+app.use('/nav',navBar);
+
+app.use(express.static(__dirname + '/public'));
+//view engine
 app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'/views'));
+
+
+
 
 app.listen(process.env.PORT || 3000,()=>{
     console.log("Now listening for requests");
