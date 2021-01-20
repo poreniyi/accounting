fbbaeea590b8d9c2ba13d2f7d9161432bdab82b1
Poreniyi <poreniyi@gmail.com>
2021-01-20 18:05:25 -0500

    fixed pathing to correctly find login.html

--- expressApp/routes/login.js
+++ expressApp/routes/login.js
@@ -2,8 +2,8 @@ let express=require('express');
 let router=express.Router();
 let path=require('path');
 
-router.get('/',(req,res,next)=>{
-   res.sendFile(path.join(__dirname,'..','public','login.html'));
+router.get('/login',(req,res,next)=>{
+   res.sendFile(path.join(__dirname,'..','public','staticHtml','login.html'));
 });
 
 module.exports=router;
