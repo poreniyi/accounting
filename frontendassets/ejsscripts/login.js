1067c6add48c82a497b5a9ef342eacf21d800bbc
Poreniyi <poreniyi@gmail.com>
2021-01-25 19:15:55 -0500

    added but commented out a fetch request to send a post request with credentials

--- frontendassets/ejsscripts/login.js
+++ frontendassets/ejsscripts/login.js
@@ -1,7 +1,26 @@
-let password=document.getElementById('pass');
+let password=document.getElementById('password');
+let username=document.getElementById('username');
 let createNewUser=document.getElementById('newUser');
 let forgotPasswordButton=document.getElementById('forgotPassword');
+let form=document.getElementById('form')
+let data={
+    password:password.value,
+    username:username.value,
+}
 
+
+// let xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange=() => {
+//     if 
+// }
+// form.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     fetch('/login'),{
+//         method:'Post',
+//         body:JSON.stringify(data),
+//         credentials:'include',
+//     }
+// })
 console.log(createNewUser);
 console.log(password);
 
