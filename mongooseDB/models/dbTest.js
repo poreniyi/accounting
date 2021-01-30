c45694bdf2f6610e4a08e98aabea81c4b0a5ef29
Poreniyi <poreniyi@gmail.com>
2021-01-30 17:53:57 -0500

    added dotenv for enviromental variables

--- mongooseDB/models/dbTest.js
+++ mongooseDB/models/dbTest.js
@@ -1,8 +1,9 @@
+let path = require('path')
+require('dotenv').config({path:path.resolve(__dirname,'../../.env')});
 let mongoose = require('mongoose');
 let Users= require('./userModels');
-//const uri = "mongodb://user1:user1@cluster0.ib9si.mongodb.net/AppDomainDatabase?retryWrites=true&w=majority";
-//const uri = "mongodb+srv://user1:user1@cluster0.ib9si.mongodb.net/Data2?retryWrites=true&w=majority";
-var uri = "mongodb://user1:user1@cluster0-shard-00-00.ib9si.mongodb.net:27017,cluster0-shard-00-01.ib9si.mongodb.net:27017,cluster0-shard-00-02.ib9si.mongodb.net:27017/Data2?ssl=true&replicaSet=atlas-lcv1f0-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+
 
 let sampleUser={
     username:'Sampple2',
@@ -17,7 +18,7 @@ mongoose.connection.on('error',()=>{
     console.log(err);
 })
 
- mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology:true },()=>{
+ mongoose.connect(process.env.mongo_uri, { useNewUrlParser: true,useUnifiedTopology:true },()=>{
     //console.log(mongoose.connection);
  });
 
@@ -26,10 +27,10 @@ mongoose.connection.on('error',()=>{
     console.log(`Connection starting`);
 })
 mongoose.connection.on('open',()=>{
-    console.log(`Connection open`);
-    Users.create(sampleUser,(err,result)=>{
-        console.log(result);
-    });
+    console.log(`Connection opened to thee database`);
+    // Users.create(sampleUser,(err,result)=>{
+    //     console.log(result);
+    // });
 })
 mongoose.connection.on('disconnected',()=>{
     console.log(`Connection closed`);
