9851fbbee735e141c02df0d6876719cf657e2c69
Poreniyi <poreniyi@gmail.com>
2021-02-01 16:52:56 -0500

    added connect-mongo as a session store

--- app.js
+++ app.js
@@ -5,25 +5,20 @@ let express= require('express');
 let path= require('path');
 let passport= require('passport');
 require('./passportSetup/passportConfig')(passport);
-let passportLocal = require('passport-local');
 let session = require("express-session");
-let asyncConnection= require('./database/DBConnection').asyncConnection;
-let Users=require('./mongooseDB/models/userModels');
-//create a passport setup file
 
 
 var app = express();
 
 //database connection goes here
 let mongoose=require('mongoose');
+const MongoStore= require('connect-mongo')(session);
 mongoose.connection.on('connecting',()=>{
     `Connected to the mongo database`;
 })
 mongoose.connection.on('error',()=>{
     console.log(err);
 })
-
-
 mongoose.connect(process.env.mongo_uri, { useNewUrlParser: true,useUnifiedTopology:true },()=>{
  });
  mongoose.connection.on('open',()=>{
@@ -35,22 +30,7 @@ mongoose.connection.on('disconnected',()=>{
 mongoose.connection.on('disconnecting',()=>{
     console.log(`Connection closed`);
 })
-//passport setup goes here
- asyncConnection.on('connect', () =>{
-     console.log(`Connected to database`);
- })
-
-asyncConnection.on('error', function (error){
-    console.log(error.toString());
-})
-
-asyncConnection.on('end',()=>{
-    //console.log('Connection to the database closed');
-    console.log(`Attempting to reconnect to the database`);
-    asyncConnection.connect();
-
-
-})
+ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -62,10 +42,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.set('trust proxy', 1) // trust first proxy
-app.use(session({ 
+app.use(session({
+    store:new MongoStore({mongooseConnection:mongoose.connection}), 
     secret: "cats",
-resave:true,
-saveUninitialized:false }));
+    resave:true,
+    saveUninitialized:false,
+    maxAge:2*24*60*60*1000 }));// days*hours*minutes*seconds*miliseonds
 app.use(passport.initialize());
 app.use(passport.session());
 
