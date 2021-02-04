267473ed00fe78cbef99e1e4df483f21ae17212b
Poreniyi <poreniyi@gmail.com>
2021-02-03 20:19:33 -0500

    added is logged in and is admin to admin index router

--- routes/adminpage/index.js
+++ routes/adminpage/index.js
@@ -1,8 +1,17 @@
 let express = require('express');
 let router = express.Router();
  
+let isLoggedIn=require('../homepage/authentication').isLoggedIn;
 
-
+let isAdmin=(req,res,next)=>{
+    if (req.session.userType.toLowerCase()=='admin'){
+        next();
+    }else{
+        res.status(403).render(`home/denied`);//this needs to be last
+    }
+}
+router.use(isLoggedIn);
+router.use(isAdmin);
 
 router.use("/", require("./userreports"));
 router.use("/", require("./dashboard"));
