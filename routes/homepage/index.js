39d6a0dbf42bedafaab761c7412a3ca701f33c51
Poreniyi <poreniyi@gmail.com>
2021-02-08 18:42:32 -0500

    passing in usertype to res.locals

--- routes/homepage/index.js
+++ routes/homepage/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 //route the user to home from root("/") request
 router.use(function(req,res, next){
     res.locals.currentUser = req.user;
+    res.locals.userType= req.session.userType;
 
     next();
 });
