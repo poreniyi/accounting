93736fa4b6c722b539ee650615880d2b10e555de
Poreniyi <poreniyi@gmail.com>
2021-02-10 12:35:13 -0500

    made routes for chartofAccounts

--- routes/accountantpages/chartOfAccounts.js
+++ routes/accountantpages/chartOfAccounts.js
@@ -4,10 +4,29 @@ let router = express.Router();
 
 
 router.get('/chart',(req,res)=>{
-    res.send('chart of accounts');
+    res.locals.hi='Hello'
+    res.render('charts/adminChart');
 });
 
 
+router.put('/edit',(req,res)=>{
+    req.body.Credit+=5000;
+    res.send(req.body);
+})
 
+router.post('/edit',(req,res)=>{
+    let array=[  sampleJson={
+        Name:"Account1",
+        Debit: 500
+    }]
+    console.log(req.body);
+  array.push(req.body);
+    res.send(array)
+})
+
+router.put('/deactivate',(req,res) => {
+    req.body.Status='Deactivated';
+    res.send(req.body);
+})
 
 module.exports = router;
