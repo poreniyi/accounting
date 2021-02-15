97eec4b08241c42ce40482e650d4e5c9b17ce0ae
Poreniyi <poreniyi@gmail.com>
2021-02-15 18:24:32 -0500

    added a post and a comment for a DB function to be implemented later

--- routes/registeredUsers/chartOfAccounts/chartOfAccounts.js
+++ routes/registeredUsers/chartOfAccounts/chartOfAccounts.js
@@ -5,6 +5,14 @@ router.get('/viewChart',(req,res) => {
     res.render('charts/chart')
 })
 router.get('/addAccount',(req,res)=>{
+    /*Sprint2 DBFunction 1
+    returns an array of used account numbers
+    assign this array to res.locals.takenNumbers
+    */
     res.render('charts/addChart');
+})
+
+router.post('addAccount',(req,res) => {
+
 })
 module.exports = router;
