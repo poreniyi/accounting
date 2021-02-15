ec781069f3c2fb28a35802e695f11c4a2d332aee
Poreniyi <poreniyi@gmail.com>
2021-02-15 17:30:22 -0500

    removed unneccessary functions

--- frontendassets/ejsscripts/editChart.js
+++ frontendassets/ejsscripts/editChart.js
@@ -34,36 +34,4 @@ cashInput.addEventListener('change',()=>{
      console.log(typeof cash)
     cashInput.value=cash;
 })
-140,026,26.00
-let data={
-    Name:'Acount2',
-    Credit: 5000,
-}
-editButton.addEventListener('click',async() => {
-    let stringfiedDta=JSON.stringify(data);
-    let request= await fetch('./edit',{method:'PUT',body: stringfiedDta, headers: {"Content-type": "application/json; charset=UTF-8"}});
-    let json= await request.json();
-    console.log(json);
-})
-addButton.addEventListener('click',async() => {
-    let stringfiedDta=JSON.stringify(data);
-    let request= await fetch('./edit',{method:'POST',body: stringfiedDta, headers: {"Content-type": "application/json; charset=UTF-8"}});
-    let json= await request.json();
-    console.log(json);
-})
-deactivateButton.addEventListener('click',async() => {
-    let data2={
-        Name:'Acount2',
-        Credit: 5000,
-        Status:'Activated'
-    }
-    let stringfiedDta=JSON.stringify(data2);
-    let request= await fetch('./deactivate',{method:'PUT',body: stringfiedDta, headers: {"Content-type": "application/json; charset=UTF-8"}});
-    let json= await request.json();
-    console.log(json);
-})
-
-let makeChange=(location,data,type) =>{
-    
-}
 
