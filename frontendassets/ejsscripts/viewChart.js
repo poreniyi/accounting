62bbb6f68a6393be9255597ad005d4f82f6d3f8c
Poreniyi <poreniyi@gmail.com>
2021-02-15 19:29:20 -0500

    added event listener to search table on key presses

--- frontendassets/ejsscripts/viewChart.js
+++ frontendassets/ejsscripts/viewChart.js
@@ -1,6 +1,24 @@
-let viewButton=document.getElementById('viewButton');
-editButton.addEventListener('click',async() => {
-    let request= await fetch('./getChart');
-    let json= await request.json();
-    console.log(json);
+let table = document.getElementById('table');
+let input = document.getElementById('search');
+
+let tr=table.getElementsByTagName('tr');
+let td,textValue;
+let cell = tr[1].getElementsByTagName('td')[0];
+console.log(`TD1 is ${cell.textContent}`)
+console.log(tr.length);
+input.addEventListener('keyup',() =>{
+    let filter=input.value.toUpperCase();
+
+    for(let i = 0; i < tr.length; i++){
+        td= tr[i].getElementsByTagName('td')[0];
+        if(td){
+            textValue= td.textContent;
+            console.log(textValue);
+            if(textValue.toUpperCase().indexOf(filter)>-1){
+                tr[i].style.display='';
+            }else{
+                tr[i].style.display='none';
+            }
+        }
+    }
 })
