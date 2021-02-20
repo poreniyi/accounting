9001d39ac3c0eb71b392afd9c19855f6655555f6
Poreniyi <poreniyi@gmail.com>
2021-02-20 05:05:21 -0500

    removed email counting

--- email/emailExample.js
+++ email/emailExample.js
@@ -34,27 +34,10 @@ let sendEmail=async (to,subject,messageContent)=>{
     else{
         isSuccesful=true;
         console.log(`Email sent `+ data.response);
-        let today=new Date();
-        let count=await fs.readFile(path.join(__dirname,'emailCount.json'),'utf8');
-        let day=1000*60*60*24;
-        count=JSON.parse(count);
-        count.date=new Date(count.date);
-        if(today.getTime()-count.date.getTime()<day){//continue
-            count.emailsSent++;
-            let writeData=JSON.stringify(count);
-            fs.writeFile(path.join(__dirname,'/emailCount.json'),writeData,'utf8');
-        }else{
-            count.emailsSent=0;
-            count.date=today;
-            fs.writeFile(path.join(__dirname,'./emailCount.json'),writeData,'utf8');
-            console.log('false');
-        }
     }
     })
     return isSuccesful;
 }
-
-//sendEmail('poreniyi@gmail.com,','account created','your account has been created'); //5 sent
 module.exports={
     sendEmail:sendEmail,
 }
