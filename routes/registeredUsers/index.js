e9552dcaa95655e1f0a297eeefb65a64f72f1445
Poreniyi <poreniyi@gmail.com>
2021-02-22 15:02:58 -0500

    added back isActivated after fixing

--- routes/registeredUsers/index.js
+++ routes/registeredUsers/index.js
@@ -1,11 +1,11 @@
 let express = require('express');
 let router = express.Router();
-let isActivated=require('../homepage/authentication').isActivated;
 
+let isActivated = require('../homepage/authentication').isActivated;
 let isLoggedIn=require('../homepage/authentication').isLoggedIn;
 
+router.use(isActivated);
 router.use(isLoggedIn);
-//router.use(isActivated);
 
 
 router.use("/", require("./dashboard/dasboard"));
