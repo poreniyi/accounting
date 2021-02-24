b6b1e7274cb15979c35df3e3a0691dcb71edf4d7
Khalid <diab@example.com>
2021-02-24 05:22:33 +0000

    ejs changes and admin perms for chart of accounts

--- routes/registeredUsers/chartOfAccounts/index.js
+++ routes/registeredUsers/chartOfAccounts/index.js
@@ -2,7 +2,14 @@ let express = require('express');
 let router = express.Router();
 router.use('/', require('./chartOfAccounts'));
 
-
+let isAdmin=(req,res,next)=>{
+    if (req.session.userType.toLowerCase()=='admin'){
+        next();
+    }else{
+        res.status(403).render(`home/denied`);
+    }
+}
+router.use(isAdmin);
 
 
 module.exports= router;
