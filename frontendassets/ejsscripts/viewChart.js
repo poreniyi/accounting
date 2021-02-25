a37ee5702891d4a7ac8cb995095d8ed60f0f961a
Poreniyi <poreniyi@gmail.com>
2021-02-24 21:30:12 -0500

    added sorting table columns

--- frontendassets/ejsscripts/viewChart.js
+++ frontendassets/ejsscripts/viewChart.js
@@ -41,3 +41,56 @@ input.addEventListener('keyup',() =>{
         }
     }
 })
+
+let headers=[...document.getElementsByTagName('TH')];
+for(let i=0;i<headers.length;i++){
+    let isClicked=1;
+    headers[i].addEventListener('click',()=>{  
+    if(isClicked){
+        headers[i].textContent=headers[i].textContent.slice(0,-1)+'\u25B2';
+        isClicked=0
+    } else{
+        headers[i].textContent=headers[i].textContent.slice(0,-1)+'\u25BC';
+        isClicked=1;
+    }
+    sortUsernames(i);
+    })
+}
+
+let sortUsernames=(n)=>{
+    var  rows, switching, i, x, y, shouldSwitch,switchcount=0;
+    let dir='asc';
+ switching=true;
+    while(switching){
+        switching = false;
+        rows=table.rows;
+        for(i=1;i<(rows.length-1);i++){
+            shouldSwitch=false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            let xval=x.textContent.toLowerCase();
+            let yval=y.textContent.toLowerCase();
+            if(dir=='asc'){
+                if(xval>yval){
+                    shouldSwitch=true;
+                    break;
+                }
+            }else if (dir=='desc'){
+                if(xval<yval){
+                    shouldSwitch=true;
+                    break;
+                }
+            }    
+        }
+        if(shouldSwitch){
+            rows[i].parentNode.insertBefore(rows[i+1],rows[i]);
+            switching=true;
+            switchcount++;
+        }else{
+            if(switchcount==0 &&dir=='asc'){
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }  
+}
