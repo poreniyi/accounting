2df1d848f21e1cc5b066b6496d816cbc153571a7
Poreniyi <poreniyi@gmail.com>
2021-03-07 14:43:01 -0500

    created post route for journal

--- routes/registeredUsers/transactions/journals.js
+++ routes/registeredUsers/transactions/journals.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/journal', (req,res) => {
+router.get('/journal', (req, res) => {
     res.render('transactions/journal')
+})
+router.post('/journal', (req, res) => {
+    //res.render('');
+    res.send(req.body);
 })
 
 module.exports = router;
