f4fc2b85bf064383a3343a6284aea366620ebe4c
Poreniyi <poreniyi@gmail.com>
2021-03-08 08:51:17 -0500

    created routes to reach createJournal

--- routes/registeredUsers/transactions/journals.js
+++ routes/registeredUsers/transactions/journals.js
@@ -4,7 +4,10 @@ let router = express.Router();
 router.get('/journal', (req, res) => {
     res.render('transactions/journal')
 })
-router.post('/journal', (req, res) => {
+router.get('/createJournal', (req, res) => {
+    res.render('transactions/addJournal');
+})
+router.post('/createJournal', (req, res) => {
     //res.render('');
     res.send(req.body);
 })
