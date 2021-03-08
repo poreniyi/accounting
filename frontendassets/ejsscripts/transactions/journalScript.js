07ce9dc68b3b8e5cf0503534c11c980746f0277d
Poreniyi <poreniyi@gmail.com>
2021-03-08 14:55:50 -0500

    added datalist for all account name inputs and prevention of form if there is an error with account names present

--- frontendassets/ejsscripts/transactions/journalScript.js
+++ frontendassets/ejsscripts/transactions/journalScript.js
@@ -5,29 +5,50 @@ let totalDebit = document.getElementById('totalDebit');
 let totalCredit = document.getElementById('totalCredit');
 let balanceMessage = document.getElementById('balanceMessage');
 let form = document.getElementById('form');
-let submitButton=document.getElementById('submitButton');
+let submitButton = document.getElementById('submitButton');
 let firstRow = htmlTable.rows[1];
-let resetButton=document.getElementById('resetButton');
-
-form.addEventListener('reset',()=>{
-    totalCredit.textContent='';
-    totalDebit.textContent='';
-    balanceMessage.textContent='';
-    balanceMessage.style.backgroundColor='';
+let resetButton = document.getElementById('resetButton');
+let isValidJournal = false;
+form.addEventListener('reset', (e) => {
+    // totalCredit.textContent = '';
+    // totalDebit.textContent = '';
+    // balanceMessage.textContent = '';
+    // balanceMessage.style.backgroundColor = '';
+    e.preventDefault();
+    let val = firstRow.cells[1].children[0].value;
+    let isCorrectName = isValidAccount(val)
+    if (!isCorrectName) {
+        alert('invalid Account Name');
+    } else {
+        console.log(`submittede`);
+    }
+    return val;
+})
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let val = firstRow.cells[1].children[0].value;
+    let isCorrectName = isValidAccount(val)
+    if (!isCorrectName) {
+        alert('invalid Account Name');
+    } else if (!isValidJournal) { 
+        alert('fix errors');
+    }
+    else {
+        form.submit();
+    }
+    return val;
 })
 
-firstRow.cells[2].children[0].addEventListener('keyup', () => {
+
+
+firstRow.cells[2].children[0].addEventListener('input', () => {
     addTotal('Debits', totalDebit);
 })
-firstRow.cells[2].children[0].addEventListener('change', () => {
-    addTotal('Debits', totalDebit);
-})
-firstRow.cells[3].children[0].addEventListener('keyup', () => {
+
+firstRow.cells[3].children[0].addEventListener('input', () => {
     addTotal('Credits', totalCredit);
 })
-firstRow.cells[3].children[0].addEventListener('change', () => {
-    addTotal('Credits', totalCredit);
-})
+
 addAccount.addEventListener('click', (e) => {
     e.preventDefault();
     if (htmlTable.rows.length == 11) {
@@ -54,26 +75,22 @@ addAccount.addEventListener('click', (e) => {
     let accountInput = document.createElement('input');
     creditInput.name = 'Credits';
     creditInput.type = 'number';
-    creditInput.min=0;
+    creditInput.min = 0;
     debitInput.type = 'number';
     debitInput.name = 'Debits';
-    debitInput.min=0;
+    debitInput.min = 0;
     accountInput.type = 'text';
     accountInput.name = 'Account';
-    accountInput.required= true;
+    accountInput.setAttribute('list', 'AccountNamesList')
+    accountInput.list = 'AccountNamesList';
+    accountInput.required = true;
     credit.appendChild(creditInput);
     debit.appendChild(debitInput);
     account.appendChild(accountInput);
-    credit.addEventListener('keyup', () => {
+    credit.addEventListener('input', () => {
         addTotal('Credits', totalCredit);
     })
-    credit.addEventListener('change', () => {
-        addTotal('Credits', totalCredit);
-    })
-    debit.addEventListener('keyup', () => {
-        addTotal('Debits', totalDebit);
-    })
-    debit.addEventListener('change', () => {
+    debit.addEventListener('input', () => {
         addTotal('Debits', totalDebit);
     })
 })
@@ -91,50 +108,64 @@ addTotal = (column, accumulator) => {
 
 let makeBalanceMessage = () => {
     let balance = Number(totalCredit.textContent) - Number(totalDebit.textContent)
-    let message1,message3,message2;
-    let isCorrectLength,hasValue,isBalanced;
-    if(htmlTable.rows.length==3){
+    let message1, message3, message2;
+    let isCorrectLength, hasValue, isBalanced;
+    if (htmlTable.rows.length == 3) {
         message1 = "Please add another account";
-        isCorrectLength=false;
-    }else {
+        isCorrectLength = false;
+    } else {
         message1 = "";
-        isCorrectLength=true
+        isCorrectLength = true
     }
-    for(let i=1;i<htmlTable.rows.length-1;i++){   
-     let row=htmlTable.rows[i].cells;
-        let credit=row[3].children[0];
-        let debit=row[2].children[0];
-        let debitValue=Number(debit.value);
-        let creditValue=Number(credit.value);
-        if(!debitValue&&!creditValue){
-            hasValue=false;
+    for (let i = 1; i < htmlTable.rows.length - 1; i++) {
+        let row = htmlTable.rows[i].cells;
+        let credit = row[3].children[0];
+        let debit = row[2].children[0];
+        let debitValue = Number(debit.value);
+        let creditValue = Number(credit.value);
+        if (!debitValue && !creditValue) {
+            hasValue = false;
             message2 = "Make sure every account has at least 1 debit or credit";
             console.log(`credit:${creditValue} debit:${debitValue}`);
-            console.log(`row${i+1} is the problem`);
-        }else{
-            message2='';
-            hasValue=true;
+            console.log(`row${i + 1} is the problem`);
+        } else {
+            message2 = '';
+            hasValue = true;
         }
     } if (balance > 0) {
-        isBalanced=false;
+        isBalanced = false;
         message3 = `Credits are ${balance} higher than Debits
         \n Please check the credit and debit values`
     } else if (balance < 0) {
-        isBalanced=false;
+        isBalanced = false;
         message3 = `Credits are ${balance}lower than Debits
         \n Please check the credit and debit values`
     } else {
-        isBalanced=true;
+        isBalanced = true;
         message3 = "";
     }
-    let isValidJournal=isCorrectLength&&hasValue&&isBalanced? true:false;
-    if(isValidJournal){
-        submitButton.style.visibility='visible';
-        balanceMessage.style.backgroundColor='green';
+    isValidJournal = isCorrectLength && hasValue && isBalanced ? true : false;
+    if (isValidJournal) {
+        submitButton.style.visibility = 'visible';
+        balanceMessage.style.backgroundColor = 'green';
         balanceMessage.textContent = "\u2713 Can be submitted";
-    }else{
-        submitButton.style.visibility='hidden';
-        balanceMessage.style.backgroundColor='red';
-        balanceMessage.textContent=message1+message2+message3;
+    } else {
+        submitButton.style.visibility = 'hidden';
+        balanceMessage.style.backgroundColor = 'red';
+        balanceMessage.textContent = message1 + message2 + message3;
     }
+}
+
+let isValidAccount = () => {
+    let accountInputs = [...document.querySelectorAll("input[name='Account']")];
+    let isValid = true;
+    accountInputs.forEach(element => {
+        console.log(element.value);
+        var option = document.querySelector("#" + 'AccountNamesList' + " option[value='" + element.value + "']");
+        if (element.value && option == null) {
+            isValid = false;
+            return;
+        }
+    })
+    return isValid;
 }
