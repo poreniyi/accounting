1a5604c66d6b1e79d68f6a7e1ff2ad256a9757e0
Poreniyi <poreniyi@gmail.com>
2021-03-09 16:49:59 -0500

    created a script for journal.ejs

---
+++ frontendassets/ejsscripts/transactions/viewJournal.js
@@ -0,0 +1,7 @@
+let textHeaders=document.getElementsByClassName('textHeader');
+let dateHeader=document.getElementsByTagName('date');
+let table=document.getElementById('table');
+
+let filterDate=()=>{
+
+}
