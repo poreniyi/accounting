212f4c6cdbb811fb61dc5c795909ca5519b443e7
Poreniyi <poreniyi@gmail.com>
2021-03-11 18:59:31 -0500

    added filters

---
+++ frontendassets/ejsscripts/transactions/ledgerScript.js
@@ -0,0 +1,51 @@
+
+let table = document.getElementById('table');
+let search = document.getElementById('search');
+let select = document.getElementById('select');
+let dateRanges = [...document.getElementsByClassName('dateRange')];
+let tr = [...table.rows];
+let searchCategory = select[select.selectedIndex].value;
+search.placeholder = select[select.selectedIndex].textContent;
+
+select.addEventListener('change', () => {
+    searchCategory = select[select.selectedIndex].value;
+    let placeholder = select[select.selectedIndex].textContent;
+    search.placeholder = placeholder;
+})
+
+dateRanges.forEach(element => {
+    element.addEventListener('change', () => {
+        let date1 = new Date(dateRanges[0].value);
+        let date2 = new Date(dateRanges[1].value);
+        for (let i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName('td')[0];
+            if (td) {
+                let selectedDate = new Date(td.textContent);
+                if (date1.getTime() < selectedDate.getTime() < date2.getTime()) {
+                    tr[i].style.display = '';
+                } else {
+                    tr[i].style.display = 'none';
+                }
+            }
+        }
+    })
+})
+let filterDate = () => {
+
+}
+search.addEventListener('keyup', () => {
+    let filter = search.value.toUpperCase();
+    for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName('td')[searchCategory-1];
+        if (td) {
+            textValue = td.textContent;
+            if (textValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].classList.toggle('transactions', false);
+                tr[i].style.display = '';
+            } else {
+                tr[i].classList.toggle('transactions', true);
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+})
