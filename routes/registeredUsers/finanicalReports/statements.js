5191f204558cdbcc1eb23ce596aa9045e09e99d6
Poreniyi <poreniyi@gmail.com>
2021-03-27 16:01:15 -0400

    created routes for balance sheet,retained,earnings,and incomestatement

--- routes/registeredUsers/finanicalReports/statements.js
+++ routes/registeredUsers/finanicalReports/statements.js
@@ -1,13 +1,21 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/statements',(req,res)=>{
-  //  res.send('hi');
+router.get('/', (req, res) => {
     res.render('statementViews/accountStatements');
 })
 
-router.get('/trialBalance', (req,res)=>{
+router.get('/trialBalance', (req, res) => {
     res.render('statementViews/trialBalance');
+})
+router.get('/balanceSheet', (req, res) => {
+    res.render('statementViews/balanceSheet');
+})
+router.get('/retainedEarnings', (req, res) => {
+    res.render('statementViews/retainedEarnings');
+})
+router.get('/IncomeStatement', (req, res) => {
+    res.render('statementViews/incomeStatement');
 })
 
 module.exports = router;
