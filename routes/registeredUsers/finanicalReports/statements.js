2ded55a41cd2658e1a51122b27872c674e9f2802
Poreniyi <poreniyi@gmail.com>
2021-03-27 15:40:30 -0400

    changed name of routes for financial statements

--- routes/registeredUsers/finanicalReports/statements.js
+++ routes/registeredUsers/finanicalReports/statements.js
@@ -1,6 +1,11 @@
 let express = require('express');
 let router = express.Router();
 
+router.get('/statements',(req,res)=>{
+  //  res.send('hi');
+    res.render('statementViews/accountStatements');
+})
+
 router.get('/trialBalance', (req,res)=>{
     res.render('statementViews/trialBalance');
 })
--- routes/registeredUsers/index.js
+++ routes/registeredUsers/index.js
@@ -11,7 +11,7 @@ router.use(isLoggedIn);
 router.use("/", require("./dashboard/dasboard"));
 router.use("/",require('./chartOfAccounts'));
 router.use("/",require('./transactions'));
-router.use("/",require('./statementSheets'));
+router.use("/",require('./finanicalReports'));
 
 
 
