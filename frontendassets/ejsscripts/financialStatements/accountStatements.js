4c6b660bd849e4ad16b059da64f9623f6fd96a77
Poreniyi <poreniyi@gmail.com>
2021-03-29 18:55:38 -0400

    added prevention of default form submit if manual option is not selected

--- frontendassets/ejsscripts/financialStatements/accountStatements.js
+++ frontendassets/ejsscripts/financialStatements/accountStatements.js
@@ -1,1 +1,23 @@
-console.log('hi');
+let form = document.getElementById('form');
+let periodSelect = document.getElementById('periodSelect')
+let periods = document.getElementById('periods');
+let dateIsManual = false;
+
+let newStart,newEnd;
+
+
+periodSelect.addEventListener('change', () => {
+    let currentVal = periodSelect.options[periodSelect.selectedIndex].value
+    console.log(currentVal)
+    if (currentVal == 'Manual') {
+        dateIsManual=true
+        console.log('hi')
+    }else dateIsManual=false
+})
+
+
+form.addEventListener('submit', (e) => {
+    if (!dateIsManual) {
+        e.preventDefault();
+    } else form.submit()
+})
