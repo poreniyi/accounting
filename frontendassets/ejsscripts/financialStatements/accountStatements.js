2e814922ab53ae393858a2e9f1b5c6d7d16c4b16
Poreniyi <poreniyi@gmail.com>
2021-03-29 19:30:03 -0400

    added date change for yearly period

--- frontendassets/ejsscripts/financialStatements/accountStatements.js
+++ frontendassets/ejsscripts/financialStatements/accountStatements.js
@@ -2,22 +2,70 @@ let form = document.getElementById('form');
 let periodSelect = document.getElementById('periodSelect')
 let periods = document.getElementById('periods');
 let dateIsManual = false;
+let startDate = document.getElementById('startDate')
+let endDate = document.getElementById('endDate')
 
-let newStart,newEnd;
+let newStart, newEnd;
 
 
 periodSelect.addEventListener('change', () => {
     let currentVal = periodSelect.options[periodSelect.selectedIndex].value
-    console.log(currentVal)
     if (currentVal == 'Manual') {
-        dateIsManual=true
-        console.log('hi')
-    }else dateIsManual=false
+        dateIsManual = true
+    } else dateIsManual = false
+    switch (currentVal) {
+        case "Maunal":
+            dateIsManual = true
+            makeManualDate();
+            break;
+        case "Yearly":
+            makeYearDate();
+            break;
+        case "Quarterly":
+        case "Monthly":
+        case "Weekly":
+    }
 })
 
 
 form.addEventListener('submit', (e) => {
-    if (!dateIsManual) {
-        e.preventDefault();
-    } else form.submit()
+    // if (!dateIsManual) {
+    //     e.preventDefault();
+    // } else form.submit()
 })
+
+let makeYearDate = () => {
+    console.log(periods)
+    document.getElementById('quarters').style.display = 'none'
+    document.getElementById('months').style.display = 'none'
+    document.getElementById('weeks').style.display = 'none'
+    startDate.disabled = true
+    endDate.disabled = true
+    let yearSelect = document.getElementById('years');
+    let yearValue = yearSelect.options[yearSelect.selectedIndex].value
+    startDate.value = `${yearValue}-01-01`
+    endDate.value = `${yearValue}-12-31`; yearSelect.addEventListener('change', () => {
+        yearValue = yearSelect.options[yearSelect.selectedIndex].value
+        startDate.value = `${yearValue}-01-01`
+        endDate.value = `${yearValue}-12-31`;
+
+    })
+
+}
+
+let makeQuarterDate = () => {
+
+}
+let makeMonthDate = () => {
+
+}
+
+let makeManualDate=()=>{
+    document.getElementById('years').style.display = 'none'
+    document.getElementById('quarters').style.display = 'none'
+    document.getElementById('months').style.display = 'none'
+    document.getElementById('weeks').style.display = 'none'
+    startDate.disabled = false
+    endDate.disabled = false
+    console.log('am I being run')
+}
