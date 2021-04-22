f8c1d09a8be1037241082db609fab21b3be8c88a
Khalid <kdiab.kde@gmail.com>
2021-04-22 00:30:08 -0400

    close accounts functionality

--- routes/registeredUsers/finanicalReports/statements.js
+++ routes/registeredUsers/finanicalReports/statements.js
@@ -6,60 +6,67 @@ router.get('/', (req, res) => {
     res.render('statementViews/accountStatements');
 })
 
-router.post('/',(req,res)=>{
-    req.session.statement={
-        start:req.body.startDate,
-        end:req.body.endDate,
+router.post('/', (req, res) => {
+    req.session.statement = {
+        start: req.body.startDate,
+        end: req.body.endDate,
     }
     res.redirect(`${req.originalUrl}/${req.body.Statement}`)
 })
 router.get('/trialBalance', async (req, res) => {
-   let data=await statements.generateTrialBalance()
+    let data = await statements.generateTrialBalance()
     try {
         res.render('statementViews/trialBalance', data);
     } catch (error) {
         res.render(`statementViews/emptyStatement`);
     }
-    
+
 })
 router.get('/balanceSheet', async (req, res) => {
     const reducer = (accumulator, currentValue) => {
         let value;
         if (typeof currentValue.BALANCE == 'string') {
-            value = currentValue.BALANCE.replace(/\W+/g,'');
-            value=-Number(value);
-        }else value=currentValue.BALANCE
+            value = currentValue.BALANCE.replace(/\W+/g, '');
+            value = -Number(value);
+        } else value = currentValue.BALANCE
         return accumulator += value;
     }
     let data = await statements.generateBalanceSheet(req.session.statement.start, req.session.statement.end, null, null, null)
 
     let totals = {
-        asset:data.asset.TextRow.reduce(reducer, 0),
-        liability:data.liability.TextRow.reduce(reducer, 0),
-        equity:data.equity.TextRow.reduce(reducer, 0)
+        asset: data.asset.TextRow.reduce(reducer, 0),
+        liability: data.liability.TextRow.reduce(reducer, 0),
+        equity: data.equity.TextRow.reduce(reducer, 0)
     }
-    res.render('statementViews/balanceSheet', {data,totals});
+    res.render('statementViews/balanceSheet', { data, totals });
 })
 router.get('/retainedEarnings', async (req, res) => {
     let RETAINEDEARNINGS = await statements.generateRetainedEarnings()
     console.log(RETAINEDEARNINGS)
-    res.render('statementViews/retainedEarnings', {RETAINEDEARNINGS});
+    res.render('statementViews/retainedEarnings', { RETAINEDEARNINGS });
 })
 router.get('/IncomeStatement', async (req, res) => {
     const reducer = (accumulator, currentValue) => {
         let value;
         if (typeof currentValue.BALANCE == 'string') {
-            value = currentValue.BALANCE.replace(/\W+/g,'');
-            value=-Number(value);
-        }else value=currentValue.BALANCE
+            value = currentValue.BALANCE.replace(/\W+/g, '');
+            value = -Number(value);
+        } else value = currentValue.BALANCE
         return accumulator += value;
     }
     let data = await statements.generateIncomeStatement()
     let totals = {
-        revenue:data.revenue.TextRow.reduce(reducer, 0),
-        expense:data.expense.TextRow.reduce(reducer, 0),
+        revenue: data.revenue.TextRow.reduce(reducer, 0),
+        expense: data.expense.TextRow.reduce(reducer, 0),
     }
-    res.render('statementViews/incomeStatement', {data,totals});
+    res.render('statementViews/incomeStatement', { data, totals });
+})
+router.get('/closeAccounts', async (req, res) => {
+    res.render('statementViews/closeAccounts')
+})
+
+router.post('/closeAccounts', async (req, res) => {
+    res.send('closed')
 })
 
 module.exports = router;
